f5351b02e04fa6c6773af13976c4ad2322b98e24
Vaibhav Malhotra <vaibhav@Vaibhavs-MacBook-Air.local>
2024-10-06 12:42:07 -0400

    Campaign Data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -8,14 +8,13 @@ import { useNavigate } from 'react-router-dom';
 
 
 
-
 const Dashboard = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const user = useSelector((state) => state.user); // Access the global user data
   const navigate = useNavigate();
   const [viewInitialData, setViewInitialData] = useState(false);
   const [viewOptimizedData, setViewOptimizedData] = useState(false);
-
+  const [activeTab, setActiveTab] = useState('Campaigns');
   // Toggle dropdown visibility
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
@@ -36,7 +35,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard-page">
       {/* Navbar */}
-      {/* <nav className="navbar">
+      <nav className="navbar">
         <div className="navbar-left">
           <div className="logo">
           <Navbar.Brand href="/">
@@ -63,8 +62,8 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-      </nav> */}
-      <NavbarComponent />
+      </nav>
+      {/* <NavbarComponent /> */}
 
       {/* Main Content */}
       <div className="dashboard-container">
@@ -79,11 +78,32 @@ const Dashboard = () => {
 
         {/* Tabs */}
         <div className="tabs">
-          <button className="tab active">Campaigns</button>
+      <button
+        className={`tab ${activeTab === 'Campaigns' ? 'active' : ''}`}
+        onClick={() => setActiveTab('Campaigns')}
+      >
+        Campaigns
+      </button>
+      <button
+        className={`tab ${activeTab === 'Ad Sets' ? 'active' : ''}`}
+        onClick={() => setActiveTab('Ad Sets')}
+      >
+        Ad Sets
+      </button>
+      <button
+        className={`tab ${activeTab === 'Ads' ? 'active' : ''}`}
+        onClick={() => setActiveTab('Ads')}
+      >
+        Ads
+      </button>
+      <button className='btn btn-primary' style={{ position: 'absolute', right: '5%' }}onClick={() => navigate('/CampaignManager')}>Create Campaign</button>
+
+    </div>
+        {/* <div className="tabs">
+          <button className='tab active'>Campaigns</button>
           <button className="tab">Ad Sets</button>
           <button className="tab">Ads</button>
-          <button className='btn btn-primary' style={{ position: 'absolute', right: '5%' }}onClick={() => navigate('/CampaignManager')}>Create Campaign</button>
-        </div>
+        </div> */}
 
         {/* Table */}
         <div className="table-container">
@@ -105,7 +125,8 @@ const Dashboard = () => {
               </tr>
             </thead>
             <tbody>
-{ viewInitialData &&<tr>
+              {/* Campaigns */}
+              { viewInitialData && activeTab=='Campaigns' &&<tr>
                 <td>Campaign 1</td>
                 <td>$120.00</td>
                 <td>70,116</td>
@@ -118,7 +139,35 @@ const Dashboard = () => {
                 <td>$118.75</td>
                 <td>$8.56</td>
               </tr>}
-{ viewOptimizedData &&<tr>
+              { viewInitialData && activeTab=='Campaigns' &&<tr>
+                <td>Campaign 2</td>
+                <td>$120.00</td>
+                <td>61,260</td>
+                <td>24,576</td>
+                <td>$600.00</td>
+                <td>17</td>
+                <td>$35.29</td>
+                <td>$1,106.00</td>
+                <td>184.33%</td>
+                <td>$65.06</td>
+                <td>$9.79</td>
+              </tr>}
+              { viewInitialData && activeTab=='Campaigns' &&<tr>
+                <td>Campaign 3</td>
+                <td>$400.00</td>
+                <td>300,000</td>
+                <td>85,000</td>
+                <td>$2,000.00</td>
+                <td>20</td>
+                <td>$100.00</td>
+                <td>$1,500.00</td>
+                <td>75.00%</td>
+                <td>$75.00</td>
+                <td>$6.67</td>
+              </tr>}
+              
+
+              { viewOptimizedData && activeTab=='Campaigns' &&<tr>
                 <td>(Optimized) Campaign 1</td>
                 <td>$120.00</td>
                 <td>75,922</td>
@@ -131,6 +180,39 @@ const Dashboard = () => {
                 <td>$83.33</td>
                 <td>$25.72</td>
               </tr>}
+              { viewOptimizedData && activeTab=='Campaigns' &&<tr>
+                <td>Campaign 2</td>
+                <td>$120.00</td>
+                <td>76,399</td>
+                <td>33,888</td>
+                <td>$600.00</td>
+                <td>25</td>
+                <td>$24.00</td>
+                <td>$1,660.00</td>
+                <td>276.67%</td>
+                <td>$66.40</td>
+                <td>$17.71</td>
+              </tr>}
+              { viewOptimizedData && activeTab=='Campaigns' &&<tr>
+                <td>Campaign 3</td>
+                <td>$400.00</td>
+                <td>325,000</td>
+                <td>100,000</td>
+                <td>$2,000.00</td>
+                <td>21</td>
+                <td>$95.24</td>
+                <td>$1,550.00</td>
+                <td>77.50%</td>
+                <td>$73.81</td>
+                <td>$6.15</td>
+              </tr>}
+
+
+
+              
+              
+
+
             </tbody>
           </table>
         </div>
